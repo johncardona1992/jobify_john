d424988e990bfb33d3ebe32029d9c0ac9a2676b4
john <johncardona1992@gmail.com>
2023-01-26 21:48:53 -0500

    Big Sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Logo from "./Logo";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { showSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
