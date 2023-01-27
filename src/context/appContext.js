a7551f8e078d46cb98f275fcb057c2f38a42e447
john <johncardona1992@gmail.com>
2023-01-27 16:29:39 -0500

    profile1

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -145,6 +145,10 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -155,6 +159,7 @@ const AppProvider = ({ children }) => {
         setupUser,
         toggleSidebar,
         logoutUser,
+        updateUser,
       }}
     >
       {children}
